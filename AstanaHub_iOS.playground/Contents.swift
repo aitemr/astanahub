e56604a178c152da2fa7469c742a2206d6f506bb
Islam <aitemr16@gmail.com>
2024-03-20 10:13:09 +0500

    Initial Commit

---
+++ AstanaHub_iOS.playground/Contents.swift
@@ -0,0 +1,18 @@
+// #1 Задание констант
+let const1 = 100
+let const2 = 100
+
+// Присвоение переменных из констант и переменных
+var var1 = const1
+var var2 = const2
+var var3 = const1 + const2
+
+// Вывод результата
+print("\(var1) \(var2) \(var3)")
+
+
+// #2 Задание имени футболиста
+let footballerName = "Cristiano"
+
+// Вывод имени футболиста
+print(footballerName)
