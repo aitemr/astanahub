490930a85917a536d73b4c99598ac466b02b51bb
Islam <aitemr16@gmail.com>
2024-03-20 10:15:51 +0500

    task 3

--- AstanaHub_iOS.playground/Contents.swift
+++ AstanaHub_iOS.playground/Contents.swift
@@ -16,3 +16,22 @@ let footballerName = "Cristiano"
 
 // Вывод имени футболиста
 print(footballerName)
+
+// #3 Напишите программу, которая определяет, является ли введенное пользователем число четным или нечетным. если число четное написать 0 если число нечетное написать 1 Sample Input: 8 Sample Output: 0
+
+func checkEvenOdd(_ number: Int) -> Int {
+    if number % 2 == 0 {
+        return 0 // Четное число
+    } else {
+        return 1 // Нечетное число
+    }
+}
+
+// Ввод числа с клавиатуры
+print("Введите число:")
+if let input = readLine(), let number = Int(input) {
+    let result = checkEvenOdd(number)
+    print("Результат: \(result)")
+} else {
+    print("Ошибка: Введите корректное целое число.")
+}
